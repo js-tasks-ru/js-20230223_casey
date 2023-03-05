090a5dfae03c0e35816a2732107e4631f546cafa
gitVera <sunnyvivi3@gmail.com>
2023-03-05 19:23:07 +0300

    tasks 03

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const keys = path.split('.');
+ 
+  return function(obj) {
+    const retrievedValue = keys.reduce((ob, key) => {
+      return (ob ? ob[key] : undefined);
+    },
+    obj);
+    return retrievedValue;
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let count = 0;
+  const arr = string.split('');
+  const newArr = arr.map((char, i) => {
+    const prev = arr[i - 1];
+    if (prev && char !== prev) {
+      count = 1;
+    } else {
+      count += 1;
+    }
+    if (count > size) {
+      return '';
+    }
 
+    return char;
+  });
+
+  return newArr.join('');
 }
