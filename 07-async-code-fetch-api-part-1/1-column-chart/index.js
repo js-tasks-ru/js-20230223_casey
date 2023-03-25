80cea80702b5ee80e3f8ed655767e2259267a1f6
gitVera <sunnyvivi3@gmail.com>
2023-03-25 12:54:55 +0300

    column-charts

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -3,67 +3,93 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ColumnChart {
+    chartHeight = 50;
     element;
     data = null;
     isLoading = false;
+    subElements = {};
+    url = '';
 
     constructor({
       url = '',
-      range = null,
+      range = { 
+        from: new Date(), 
+        to: new Date(),
+      },
       label = '',
       formatHeading = data => data,
       link = '',
     }) {
-      this.url = url;
+      this.url = new URL(url, BACKEND_URL); 
       this.range = range;
       this.label = label;
       this.formatHeading = formatHeading;
       this.link = link;
 
       this.render();
+      this.update();
 
     }
 
     async loadData() {
+      this.url.searchParams.set('from', this.range.from.toISOString());
+      this.url.searchParams.set('to', this.range.to.toISOString());
       this.isLoading = true;
-      const data = await fetchJson(`${BACKEND_URL}/${this.url}?from=${this.range.from}&to=${this.range.to}`, this.range);
+      const data = await fetchJson(this.url);
       console.log(data);
       this.data = data;
       this.isLoading = false;
-      if(!this.isLoading) {
-        this.render();
-      }
+      return data;
     }
 
-    update(from, to) {
+    async update(from = new Date(), to = new Date()) {
+      this.element.firstElementChild.classList.add('column-chart_loading');
       this.range = {
         from,
         to,
       };
-      this.loadData();
+      const data = await this.loadData();
+      if (data && Object.values(data).length) {
+        this.subElements.header.textContent = this.getHeaderValue(data);
+        this.subElements.body.innerHTML = this.getItems(data);
+        this.element.firstElementChild.classList.remove('column-chart_loading');
+      }
+     
+      return data;
     }
 
-    getItems() {
-        if(!this.data || this.isLoading) {
-          return '';
-        }
+    getHeaderValue(data) {
+      return this.formatHeading(Object.values(data).reduce((accum, item) => (accum + item), 0));
+    }
 
-        return Object.entries(this.data)?.map(item => `<div style="--value: ${item[1]}" data-tooltip="25%"></div>`).join('');
+    getItems(data = null) {
+      if (!data || this.isLoading) {
+        return '';
+      }
+
+      const maxValue = Math.max(...Object.values(data));
+
+
+      return Object.entries(data)
+                .map(([key, value]) => {
+                  const scale = this.chartHeight / maxValue;
+                  const percent = (value / maxValue * 100).toFixed(0);
+                    
+                  return `<div style="--value: ${Math.floor(value * scale)}" data-tooltip="${percent}%"></div>`;   
+                }).join('');
     }
 
     getTemplate() {
       return `
       <div class="dashboard__chart_${this.label}">
-      <div class="column-chart ${this.isLoading ? `column-chart_loading` : ''}" style="--chart-height: 50">
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
         <div class="column-chart__title">
           Total ${this.label}
           ${this.link && `<a href="${this.link}" class="column-chart__link">View all</a>`}
         </div>
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">$243,437</div>
-          <div data-element="body" class="column-chart__chart">
-            ${this.getItems()}
-          </div>
+          <div data-element="body" class="column-chart__chart"></div>
         </div>
       </div>
     </div>
@@ -75,6 +101,24 @@ export default class ColumnChart {
       tempWrapper.innerHTML = this.getTemplate();
       const element = tempWrapper.firstElementChild; 
       this.element = element;
+
+      this.subElements = this.getSubElements(element);
+    }
+
+    getSubElements(element) {
+      const result = {};
+      const elements = element.querySelectorAll('[data-element]');
+    
+      for (const subElement of elements) {
+        const name = subElement.dataset.element;
+        result[name] = subElement;
+      }
+    
+      return result;
+    }
+
+    destroy() {
+        this.element.remove();
     }
 
 
